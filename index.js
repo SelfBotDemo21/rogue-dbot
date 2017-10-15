fa9e1b9a425a24390cbd5724dc106bcb87906c10
goddy <onememeonly@gmail.com>
2017-10-14 22:51:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ r;ship - Ships 2 users. (args required: 1)
 \`message.author.username\`
 \`message.content\`
 
-Relationship Status: ${relStat[Math.floor(Math.random * relStat.length)]}`)
+Relationship Status: ${relStat[Math.floor(Math.random() * relStat.length)]}`)
 });
 
 client.login(process.env.BOT_TOKEN);
