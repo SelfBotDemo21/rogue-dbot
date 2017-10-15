23e1cf1770452abd2e70fa449096a481f393cc58
goddy <onememeonly@gmail.com>
2017-10-14 22:46:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,30 @@ client.on('ready', () => {
 
 const prefix = "r;";
 
+const relStat = [
+  "100% - Amazing!", "75% - Not as good, but still strong", "50% - Eh. It has it's upsides and downsides", "35% - Poor", "25% - Really poor", "10% - Bad"
+]
+
 client.on('message', msg => {
   if (msg.content.startsWith(prefix + 'ping')) {
     msg.channel.send("Pinging... ❤").then(m => {
       m.edit(`Pong! - Time Taken: ${m.createdTimestamp - msg.createdTimestamp}ms`)
     })
   }
-
+  
   if (msg.content.startsWith(prefix + 'help')) {
     msg.author.send(`List of commands:\n
-r;ping - Checks if I'm still responding (w/ response time)`)
+r;ping - Checks if I'm still responding (w/ response time)
+r;ship - Ships 2 users. (args required: 1)
   }
+  
+  if (msg.content.startsWith(prefix + 'ship')) {
+    msg.channel.send(`❤ **Shipping** ❤\n
+
+\`message.author.username\`
+\`message.content\`
+
+Relationship Status: ${relStat[Math.floor(Math.random * relStat.length)]}`)
 });
 
 client.login(process.env.BOT_TOKEN);
