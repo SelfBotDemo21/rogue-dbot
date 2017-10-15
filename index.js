e82be40cac69604cedee9c5d066671bde3e0821f
goddy <onememeonly@gmail.com>
2017-10-15 02:14:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,7 @@ r;ship - Ships you with someone/something.`)
       return msg.reply(`Please provide arguments, ${msg.author.username}`)
     }
     msg.channel.send(`❤ **Shipping** ❤\n
-\`${msg.author.username}\`
-\`${args}\`
-
+${msg.author.username} x ${args}\n
 Relationship Status: **${relStat[Math.floor(Math.random() * relStat.length)]}**`)
   }
 });
